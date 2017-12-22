dbd78c4c382ddbcc44c90871724241c9e973e963
Ray950611 <rd2622@columbia.edu>
2017-12-21 21:46:02 -0500

    added my own app.py file and a test notebook

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
 from flask import Flask, render_template, request, redirect
-
+import pandas as pd
+from bokeh.embed import components
+from bokeh.resources import CDN,INLINE
+from bokeh.plotting import figure, output_file, show
 app = Flask(__name__)
 
 @app.route('/')
 def index():
   return render_template('index.html')
-
+@app.route('/result/',methods=['POST'])
+def stock_price():
+  ticker = request.form['stock']
+  price_type = request.form['type'] 
+  data = pd.read_json("https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?ticker="+str(ticker)+"&api_key=82ocY42PXsgZMnTGWcCD",orient="table")
+  table=data["datatable"]["data"]
+  time_period = 22
+  dic = {'close':5,'open':2,'high':3,'low':4}
+  k = dic[str(price_type)]
+  X = []
+  Y = []
+  for i in range(time_period):
+      cur = table[-1-i]
+      cur_date = pd.to_datetime(str(cur[1])).date()
+      cur_price = float(cur[k])
+      X.insert(0,cur_date)
+      Y.insert(0,cur_price)
+  p = figure(title="Quandl WIKI Stock Prices 2017",x_axis_label='date',x_axis_type="datetime")
+  p.line(pd.to_datetime(X), Y, legend=str(ticker)+":"+str(price_type), line_width=2)
+  script, div = components(p)
+        
+  return render_template('graph.html',script=script, div=div)
 if __name__ == '__main__':
   app.run(port=33507)
